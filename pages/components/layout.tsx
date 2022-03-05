86bca99f3ec62da7b69f3ec45de9d1c7d27b5416
seongsoo96 <seongsoo.dev@gmail.com>
2022-03-05 20:08:50 +0900

    feat: modify main layout

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -5,7 +5,9 @@ export default function Layout({ children }) {
 	return (
 		<>
 			<Navbar />
-			<main>{children}</main>
+			<main className="max-w-4xl mx-auto px-5 py-20 sm:px-6 md:px-8 xl:px-12 xl:max-w-4xl">
+				{children}
+			</main>
 			<Footer />
 		</>
 	);
