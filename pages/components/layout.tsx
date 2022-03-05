8078801ae38fe662816c807ae33842981fc94bcf
seongsoo96 <seongsoo.dev@gmail.com>
2022-03-05 20:18:38 +0900

    fix: add interface for children

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,7 +1,11 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
 
-export default function Layout({ children }) {
+interface LayoutProps {
+	children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
 	return (
 		<>
 			<Navbar />
