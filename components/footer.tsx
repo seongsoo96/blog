6845f608da29e3d6593be957a82037aa4f91eec5
seongsoo96 <seongsoo.dev@gmail.com>
2022-03-13 22:01:58 +0900

    feat: update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,15 @@
 export default function Footer() {
-	return <div>푸터입니다!!!</div>;
+	return (
+		<>
+			<footer className="my-24 text-center">
+				<div>
+					<a>인스타</a>
+					<a>블로그</a>
+					<a>포트폴리오</a>
+					<a>인스타</a>
+				</div>
+				<p>Copyright ⓒ KimDev 2022 All rights reserved.</p>
+			</footer>
+		</>
+	);
 }
