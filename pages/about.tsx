6e02ac4e7c9d993ed9416931164b7439761dd25b
seongsoo96 <seongsoo.dev@gmail.com>
2022-04-23 11:29:49 +0900

    feat: add skill-set icon in about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -157,8 +157,61 @@ export default function About() {
 			<h1 className="about-title">기술스택</h1>
 			<article>
 				<div className="content">
-					안녕하세요. 저는 @@에서 병원 솔루션을 개발하는
-					웹개발자입니다.
+					<Image
+						src="https://img.shields.io/badge/Java-007396?style=flat-square&logo=Java&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/Javascript-ffb13b?style=flat-square&logo=javascript&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=css3&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/Spring-6DB33F?style=flat-square&logo=Spring&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/Spring Boot-6DB33F?style=flat-square&logo=Spring Boot&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/Vue.js-4FC08D?style=flat-square&logo=vue.js&logoColor=white"
+						width={100}
+						height={50}
+					/>{" "}
+					<Image
+						src="https://img.shields.io/badge/Handlebar.js-000000?style=flat-square&logo=Handlebars.js&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/OracleDB-F80000?style=flat-square&logo=Oracle&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/MariaDB-003545?style=flat-square&logo=MariaDB&logoColor=white"
+						width={100}
+						height={50}
+					/>
+					<Image
+						src="https://img.shields.io/badge/MySQL-4479A1?style=flat-square&logo=MySQL&logoColor=white"
+						width={100}
+						height={50}
+					/>
 				</div>
 			</article>
 			<h1 className="about-title">학력사항</h1>
