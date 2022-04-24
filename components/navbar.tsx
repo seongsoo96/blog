dd865189088bb53fa78d194475727d041225689c
seongsoo96 <seongsoo.dev@gmail.com>
2022-04-24 16:39:11 +0900

    fix: alter position of dark mode checkbox in navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,14 +21,7 @@ const profile = [
 export default function Navbar() {
 	// const [drop, setDrop] = useState(false);
 	// const onClick = () => (drop ? setDrop(false) : setDrop(true));
-	// const { systemTheme, theme, setTheme } = useTheme();
 	const { systemTheme, theme, setTheme } = useTheme();
-	const [mounted, setMounted] = useState(false);
-	const [checked, setChecked] = useState(
-		systemTheme === "dark" ? false : true
-	);
-	useEffect(() => setMounted(true), []);
-	if (!mounted) return null;
 	const renderThemeChanger = () => {
 		const currentTheme = theme === "system" ? systemTheme : theme;
 		return (
@@ -37,9 +30,9 @@ export default function Navbar() {
 					<input
 						type="checkbox"
 						id="hide-checkbox"
-						defaultChecked={checked}
+						// defaultChecked={checked}
 						onClick={() =>
-							setTheme(currentTheme === "dark" ? "light" : "dark")
+							setTheme(theme === "dark" ? "light" : "dark")
 						}
 					/>
 					<label htmlFor="hide-checkbox" className="toggle">
@@ -62,38 +55,40 @@ export default function Navbar() {
 						<span className="star star-8"></span>
 					</label>
 				</li>
-			</>
 
-			// <button
-			// 	aria-label="Toggle Dark Mode"
-			// 	type="button"
-			// 	className="p-3 h-12 w-12 order-2 md:order-3 absolute left-2/4 transform -translate-x-2/4 lg:transform-none md:relative md:left-0"
-			// 	onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-			// >
-			// 	<svg
-			// 		xmlns="http://www.w3.org/2000/svg"
-			// 		viewBox="0 0 24 24"
-			// 		fill="currentColor"
-			// 		stroke="currentColor"
-			// 		className="h-6 w-6 text-gray-800 dark:text-gray-200"
-			// 	>
-			// 		{theme === "dark" ? (
-			// 			<path
-			// 				strokeLinecap="round"
-			// 				strokeLinejoin="round"
-			// 				strokeWidth={2}
-			// 				d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-			// 			/>
-			// 		) : (
-			// 			<path
-			// 				strokeLinecap="round"
-			// 				strokeLinejoin="round"
-			// 				strokeWidth={2}
-			// 				d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-			// 			/>
-			// 		)}
-			// 	</svg>
-			// </button>
+				{/* <button
+					aria-label="Toggle Dark Mode"
+					type="button"
+					className="p-3 h-12 w-12 order-2 md:order-3 absolute left-2/4 transform -translate-x-2/4 lg:transform-none md:relative md:left-0"
+					onClick={() =>
+						setTheme(theme === "dark" ? "light" : "dark")
+					}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						stroke="currentColor"
+						className="h-6 w-6 text-gray-800 dark:text-gray-200"
+					>
+						{theme === "dark" ? (
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+							/>
+						) : (
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+							/>
+						)}
+					</svg>
+				</button> */}
+			</>
 		);
 
 		// if (currentTheme === "dark") {
