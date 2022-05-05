5fd7ed8de5254705f35425334fbfaf662cde3555
seongsoo96 <seongsoo.dev@gmail.com>
2022-05-05 12:20:16 +0900

    feat: add comments.tsx

---
+++ components/comments.tsx
@@ -0,0 +1,20 @@
+export const Comments: React.FC = () => (
+	<section
+		ref={(elem) => {
+			if (!elem) {
+				return;
+			}
+			const scriptElem = document.createElement("script");
+			scriptElem.src = "https://utteranc.es/client.js";
+			scriptElem.async = true;
+			scriptElem.crossOrigin = "anonymous";
+			scriptElem.setAttribute("repo", "seongsoo96/comments");
+			scriptElem.setAttribute("issue-term", "title");
+			scriptElem.setAttribute("label", "comment");
+			scriptElem.setAttribute("theme", "github-dark");
+			elem.appendChild(scriptElem);
+		}}
+	/>
+);
+
+export default Comments;
