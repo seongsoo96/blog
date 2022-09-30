b3f6586dbe4ba58364954c88ed6a5496d8161b1c
seongsoo96 <gasmathdog@gmail.com>
2022-09-30 22:28:54 +0900

    feat: Add github logo to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import profileImg from '../public/profile3.jpeg';
+import githubLight from '../public/github-light.png';
+import githubDark from '../public/github-dark.png';
+import { useTheme } from 'next-themes';
 
 // import Image from "next/image";
 // import profileImg from "../public/profile2.jpeg";
@@ -26,40 +29,55 @@ const category = [
   // },
 ];
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Blog</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <div className="category-header">
-      <h1 className="category-title">G&rsquo;day Mate!</h1>
-      <p className="category-description text-2xl font-bold underline rounded-md underline-offset-4 decoration-8 decoration-indigo-500 mb-10">
-        안녕하세요&#128075; 이성수입니다!!!!
-      </p>
-      <div className="flex bg-slate-100 rounded-xl p-6 dark:bg-slate-800">
-        <div className="w-[100px] h-[100px] rounded-full border-2 border-solid border-slate-600">
-          <Image
-            src={profileImg}
-            alt="profile"
-            width={100}
-            height={100}
-            className="rounded-full"
-          />
-        </div>
-        <div className="ml-8 pt-2">
-          <p className="text-lg font-semibold">Seongsoo</p>
-          <blockquote className="mt-2">
-            <p className="font-normal">Frontend Developer</p>
-          </blockquote>
-          <figcaption className="font-medium mt-1">
-            <div className="text-slate-700 dark:text-slate-500">
-              &ldquo;어떤 말을 쓰면 좀 멋져 보일까요?&rdquo;
+const Home = () => {
+  const { systemTheme, theme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="category-header">
+        <h1 className="category-title">G&rsquo;day Mate!</h1>
+        <p className="category-description text-2xl font-bold underline rounded-md underline-offset-4 decoration-8 decoration-indigo-500 mb-10">
+          안녕하세요&#128075; 이성수입니다!!!!
+        </p>
+        <div className="grid grid-cols-[100px_auto] bg-slate-100 rounded-xl p-6 dark:bg-slate-800">
+          <div className="w-[100px] h-[100px] rounded-full border-2 border-solid border-slate-600">
+            <Image
+              src={profileImg}
+              alt="profile"
+              width={100}
+              height={100}
+              className="rounded-full"
+            />
+          </div>
+          <div className="ml-8 pt-2">
+            <div className="flex justify-between">
+              <div>
+                <p className="text-lg font-semibold">Seongsoo</p>
+                <blockquote className="mt-2">
+                  <p className="font-normal">Frontend Developer</p>
+                </blockquote>
+              </div>
+              <div>
+                <Image
+                  src={currentTheme === 'dark' ? githubDark : githubLight}
+                  alt="github logo"
+                  width={20}
+                  height={20}
+                />
+              </div>
             </div>
-          </figcaption>
+            <figcaption className="font-medium mt-1">
+              <div className="text-slate-700 dark:text-slate-500">
+                &ldquo;어떤 말을 쓰면 좀 멋져 보일까요?&rdquo;
+              </div>
+            </figcaption>
+          </div>
         </div>
-      </div>
-      {/* <div className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        {/* <div className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
         <div className="md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
@@ -87,32 +105,34 @@ const Home = () => (
           </figcaption>
         </div>
       </div> */}
-      <br />
-      <div className="md:flex bg-slate-100 rounded-xl p-6 md:p-0 dark:bg-slate-800">
-        <div className="md:p-8 md:text-left space-y-4">
-          <ul>
-            {category.map((c) => (
-              <li key={c.name} className="text-xl">
-                <Link href={c.href}>
-                  <a>
-                    <div className="pt-2 pb-4">
-                      <p>
-                        <span className="underline underline-offset-4 dark:hover:text-blue-400">
-                          {c.name}
-                        </span>{' '}
-                        - <span className="text-lg font-normal">{c.desc}</span>
-                      </p>
-                    </div>
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <br />
+        <div className="md:flex bg-slate-100 rounded-xl p-6 md:p-0 dark:bg-slate-800">
+          <div className="md:p-8 md:text-left space-y-4">
+            <ul>
+              {category.map((c) => (
+                <li key={c.name} className="text-xl">
+                  <Link href={c.href}>
+                    <a>
+                      <div className="pt-2 pb-4">
+                        <p>
+                          <span className="underline underline-offset-4 dark:hover:text-blue-400">
+                            {c.name}
+                          </span>{' '}
+                          -{' '}
+                          <span className="text-lg font-normal">{c.desc}</span>
+                        </p>
+                      </div>
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-    {/* <Comments /> */}
-  </>
-);
+      {/* <Comments /> */}
+    </>
+  );
+};
 
 export default Home;
